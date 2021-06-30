31bdbf433c3a1acb891470610011042579a40300
Ingrid Nogueira <isnogueira601@gmail.com>
2021-06-29 23:17:34 -0300

    initial commit

---
+++ src/dia0/hello.java
@@ -0,0 +1,10 @@
+package dia0;
+
+public class hello {
+
+    public static void main(String[] args) {
+
+        System.out.println("Java version: " + System.getProperty("java.version"));
+    }
+
+}
